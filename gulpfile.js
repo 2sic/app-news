be80b245be2762c5b99f06a9b74a93bcddc534c1
lucatescari <luca@ltescari.ch>
2018-04-23 09:08:21 +0200

    updated gulpfile to new standard

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,49 @@
-﻿
-var gulp = require('gulp');
+﻿var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
-var sourceFiles = ['src/*.scss'];
-
-gulp.task('watch-sass', function () {
-	gulp.watch(sourceFiles, ['sass']);
+gulp.task('watch-sass-all', function () {
+	gulp.watch(['./src/scss/*.scss'], ['sass-all']);
 });
+gulp.task('sass-all', function () {
+  return gulp.src(['./src/scss/*.scss'])
+	.pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+	.pipe(sourcemaps.write('.', { sourceRoot: '../src/scss/' }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+
+gulp.task('watch-sass-bs3', function () {
+	gulp.watch(['./src/scss/*.scss'], ['sass-bs3']);
+});
+gulp.task('sass-bs3', function () {
+  return gulp.src(['./src/scss/bs3.scss'])
+	.pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+	.pipe(sourcemaps.write('.', { sourceRoot: '../src/scss/' }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+
+gulp.task('watch-sass-bs4', function () {
+	gulp.watch(['./src/scss/*.scss'], ['sass-bs4']);
+});
+gulp.task('sass-bs4', function () {
+  return gulp.src(['./src/scss/bs4.scss'])
+	.pipe(sourcemaps.init())
+	.pipe(sass().on('error', sass.logError))
+	.pipe(sourcemaps.write('.', { sourceRoot: '../src/scss/' }))
+	.pipe(gulp.dest('./dist/'));
+});
+
 
 gulp.task('watch-javascript', function () {
-	gulp.watch(['src/**.js'], ['javascript']);
+	gulp.watch(['./src/js/*.js'], ['javascript']);
 });
-
-gulp.task('sass', function () {
-
-return gulp.src(sourceFiles)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write('.', { sourceRoot: '../src/' }))
-    .pipe(gulp.dest('dist/'));
-});
-
 gulp.task('javascript', function() {
-	return gulp.src('src/**.js')
-		.pipe(gulp.dest('dist/'));
+	return gulp.src('./src/js/*.js')
+		.pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('default', ['sass', 'javascript', 'watch-sass', 'watch-javascript']);
+gulp.task('default', ['sass-all', 'javascript', 'watch-sass-all', 'watch-javascript']);
