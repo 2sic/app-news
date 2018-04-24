e21918915988f7de66080171c8f21e381dbd491b
lucatescari <luca@ltescari.ch>
2018-04-24 16:22:46 +0200

    deleted old files

--- src/_old-ignore/script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(".app-news-categories").click(function(e) {
-        e.stopPropagation();
-    });
-    $(".app-news-item .btn.btn-primary").click(function(e) {
-        e.stopPropagation();
-    });
-});
