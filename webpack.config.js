f06b844455d51040436344effde6347879f22c11
Roman Opalko <roman.opalko@2sic.com>
2020-01-29 14:43:38 +0100

    add js minify to webpack

---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const WebpackBar = require('webpackbar');
+
+module.exports = {
+  entry: {
+    bs3: './bs3/styles/bs3.scss',
+    bs4: './src/styles/bs4.scss',
+    scripts: './src/ts/scripts.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].min.js',
+  },
+  mode: 'production',
+  devtool: 'source-map',
+  watch: true,
+  stats: {
+    all: false,
+    assets: true
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.scss']
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      }),
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorOptions: {
+          map: {
+            inline: false,
+            annotation: true,
+          }
+        }
+      })
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].min.css',
+    }),
+    new WebpackBar(),
+    new FriendlyErrorsWebpackPlugin()
+  ],
+  module: {
+    rules: [{
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          }, {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              plugins: [
+                require('autoprefixer')
+              ]
+            }
+          }, {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true
+            }
+          }
+        ],
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader'
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/'
+          }
+        }]
+      }
+    ],
+  },
+};
