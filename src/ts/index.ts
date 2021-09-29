58e4dff4074fd9186df664b36769c5a3778afdb3
Maxim Hüsler <maxim.huesler@2sic.com>
2021-09-29 10:32:29 +0200

    refactored to 2sxc 12.05

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,22 +1,10 @@
 declare let Fancybox: any;
 
-// This is only partial - if you want to see more, see https://fancyapps.com/docs/ui/fancybox/options
-interface FancyboxOptions {
-  groupAll: boolean;
-  Thumbs: {
-    autostart: boolean;
-  }
-  startIndex?: number;
-  preload?: number;
-  infinite?: boolean;
-  autoFocus?: boolean;
-}
-
 var winAny = window as any;
 winAny.appNews5 ??= {};
 winAny.appNews5.activateFancybox ??= activateFancybox;
 
-function activateFancybox({ groupId, options }: { groupId: string, options: FancyboxOptions }) {
+function activateFancybox({ groupId }: { groupId: string }) {
   console.log(groupId);
-  Fancybox.bind(`[data-app-news5-fancybox="${groupId}"]`, options);
+  Fancybox.bind(`[data-app-news5-fancybox="${groupId}"]`);
 }
