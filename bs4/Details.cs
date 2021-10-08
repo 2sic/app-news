4f1d84d018fdccccb7c7a5ca7839a8d2aa1547c2
Maxim Hüsler <maxim.huesler@2sic.com>
2021-10-08 11:15:28 +0200

    removed old HtmlPage usage

--- bs4/Details.cs
+++ bs4/Details.cs
@@ -4,25 +4,10 @@ using System.Web;
 
 public class Details: Custom.Hybrid.Code12
 {
-  public void SetNotFoundHttpHeaders() {
-    // Dnn only - this used to be done in Details if the page wasn't found
-    // We're not sure if this was a good idea, for now just disable
-    // If we plan on reactivating it, we have to pass the Response object to the code, as the code doesn't have a Response object
-    // Response.StatusCode = 404;
-    // Response.TrySkipIisCustomErrors = true;
-  }
 
   // Shows a back to list button
   public dynamic BackToListButton() {
     return Tag.A(App.Resources.LabelBackToList).Class("btn").Class("btn-outline-primary").Class("app-backtolist").Href(Link.To().ToString());
-  }
-
-  public void AddMetaTags(dynamic article) {
-    // Dnn only
-    #if !NETCOREAPP
-      HtmlPage.AddMeta("description", Tags.Strip(article.Teaser));
-      HtmlPage.Title = article.Date.ToString("d") + " - " + article.Title +  " | " + HtmlPage.Title;
-    #endif
   }
 }
 
